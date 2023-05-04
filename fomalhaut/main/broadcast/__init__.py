ca8373e11ee52792b76ccba8f91c8016ef63e5bb
AlphaKR93 <dev@alpha93.kr>
2023-05-05 01:00:17 +0900

    Fix CodeQL Action

--- fomalhaut/main/broadcast/__init__.py
+++ fomalhaut/main/broadcast/__init__.py
@@ -1,2 +1,4 @@
 from .heyst import Heyst
 from .pablo import Pablo
+
+__all__ = ['Heyst', 'Pablo']
--- fomalhaut/module/instance/handlers/message/embed/element/__init__.py
+++ fomalhaut/module/instance/handlers/message/embed/element/__init__.py
@@ -2,3 +2,5 @@ from .AuthorElement import AuthorElement
 from .ColourElement import ColourElement
 from .FieldElement import FieldElement
 from .FooterElement import FooterElement
+
+__all__ = ["AuthorElement", "ColourElement", "FieldElement", "FooterElement"]
